bcd841eef5f77bb56d08caa36956e750def51ff2
Jeremy Johnson <jeremy@softworks.com.my>
2013-10-08 12:08:06 +0800

    adding django ecommerce to this repo for use by the Adv Python book

---
+++ django_ecommerce/django_ecommerce/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'main.views.index', name='home'),
+    # url(r'^django_ecommerce/', include('django_ecommerce.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url('^pages/', include('django.contrib.flatpages.urls')),
+)
