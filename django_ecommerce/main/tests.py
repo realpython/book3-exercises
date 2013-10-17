4587449774c9c2bbc1593a61f894432a52fefeda
Jeremy Johnson <jeremy@softworks.com.my>
2013-10-18 02:29:48 +0800

    put in mock for database object

--- django_ecommerce/main/tests.py
+++ django_ecommerce/main/tests.py
@@ -6,7 +6,6 @@ from django.shortcuts import render_to_response
 
 class MainPageTests(TestCase):
 
-
     @classmethod
     def setUpClass(cls):
         from django.test import RequestFactory
@@ -15,7 +14,7 @@ class MainPageTests(TestCase):
         cls.request.session = {}
         
 
-    def test_root_resolves_to_main_view(self):
+    def test_root_resolves_to_main_view(self ):
         main_page = resolve('/')
         self.assertEqual(main_page.func, index)
 
@@ -23,34 +22,37 @@ class MainPageTests(TestCase):
         resp = index(self.request)
         self.assertEquals(resp.status_code,200)
 
-    def test_uses_index_html_template(self):
-        resp = index(self.request)
-        self.assertTemplateUsed(resp, "index.html")
-
     def test_returns_exact_html(self):
         resp = index(self.request)
         self.assertEquals(resp.content,
                           render_to_response("index.html").content)
 
     def test_index_handles_logged_in_user(self):
+        #create a session that appears to have a logged in user
+        self.request.session = {"user" : "1"}
+        
         #create the user needed for user lookup from index page
         from payments.models import User
         user = User(
                 name = 'jj',
                 email = 'j@j.com',
             )
-        user.save()
-        
-        #create a session that appears to have a logged in user
-        self.request.session = {"user" : "1"}
+        import mock
+        with mock.patch('main.views.User') as user_mock:
+            
+            #set the object function to our mock
+            user_mock.objects = mock.Mock()
 
-        #request the index page
-        resp = index(self.request)
+            #tell the mock what to do when called
+            config = {'get.return_value':mock.Mock()}
+            user_mock.objects.configure_mock(**config)
 
-        #ensure we return the state of the session back to normal so
-        #we don't affect other tests
-        self.request.session = {}
-        
-        #verify it returns the page for the logged in user
-        expectedHtml = render_to_response('user.html', {'user':user}).content
-        self.assertEquals(resp.content,expectedHtml)
+
+            #run the test
+            resp = index(self.request)
+
+            #ensure we return the state of the session back to normal 
+            self.request.session = {}
+           
+            expected_html = render_to_response('user.html',{'user': user_mock.get_by_id(1)})
+            self.assertEquals(resp.content, expected_html.content)
