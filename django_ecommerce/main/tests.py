ebe0fa1f1b2056e3ea9c04b8609dbfe0278bb499
Jeremy Johnson <jeremy@softworks.com.my>
2013-10-18 01:13:34 +0800

    Added check for full html to test_iundex_handles_logged_iun_user

--- django_ecommerce/main/tests.py
+++ django_ecommerce/main/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from .views import index
+import unittest
+from django.shortcuts import render_to_response
 
 class MainPageTests(TestCase):
 
@@ -17,15 +19,44 @@ class MainPageTests(TestCase):
         index = self.client.get('/')
         self.assertTemplateUsed(index, "index.html")
 
+    def test_uses_index_html_template_2(self):
+        #create a dummy request
+        from django.test import RequestFactory
+        request_factory = RequestFactory()
+        request = request_factory.get('/')
+        request.session = {} #make sure it has a session associated
+
+        #call the view function
+        resp = index(request)
+
+        #check the response
+        self.assertContains(resp, "<title>Your MVP</title>")
+
     def test_returns_exact_html(self):
         index = self.client.get("/")
-        from django.shortcuts import render_to_response
-        self.assertEquals(index, render_to_response("index.html"))
+        self.assertEquals(index.content,
+                          render_to_response("index.html").content)
 
-        #ok we know it's returning something
-        #ass self.assertTrue('blah' in index.context)
-        #.. but that's brital and it's a constant so add
-        #rendepr_to_response
-        #so then do with still need our resolve test?  yes look at error
-        #message
-        #then test the interaction with the user session
+    def test_index_handles_logged_in_user(self):
+        #create the user needed for user lookup from index page
+        from payments.models import User
+        user = User(
+                name = 'jj',
+                email = 'j@j.com',
+            )
+        user.save()
+        
+        #create a Mock request object, so we can manipulate the session
+        from django.test import RequestFactory
+        request_factory = RequestFactory()
+        request = request_factory.get('/')
+        #create a session that appears to have a logged in user
+        request.session = {"user" : "1"}
+
+        #request the index page
+        resp = index(request)
+
+        #verify it returns the page for the logged in user
+        #self.assertTemplateUsed(resp, 'user.html')
+        expectedHtml = render_to_response('user.html', {'user':user}).content
+        self.assertEquals(resp.content,expectedHtml)
