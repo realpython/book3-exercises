e68b5209c4db8bf5ba06cc34f056802c2bbae114
Jeremy Johnson <jeremy@softworks.com.my>
2013-10-18 01:57:57 +0800

    factored out the requestFactory to setUpClass method

--- django_ecommerce/main/tests.py
+++ django_ecommerce/main/tests.py
@@ -7,34 +7,29 @@ from django.shortcuts import render_to_response
 class MainPageTests(TestCase):
 
 
+    @classmethod
+    def setUpClass(cls):
+        from django.test import RequestFactory
+        request_factory = RequestFactory()
+        cls.request = request_factory.get('/')
+        cls.request.session = {}
+        
+
     def test_root_resolves_to_main_view(self):
         main_page = resolve('/')
         self.assertEqual(main_page.func, index)
 
     def test_returns_appropriate_html_respos_code(self):
-        index = self.client.get('/')
-        self.assertEquals(index.status_code,200)
+        resp = index(self.request)
+        self.assertEquals(resp.status_code,200)
 
     def test_uses_index_html_template(self):
-        index = self.client.get('/')
-        self.assertTemplateUsed(index, "index.html")
-
-    def test_uses_index_html_template_2(self):
-        #create a dummy request
-        from django.test import RequestFactory
-        request_factory = RequestFactory()
-        request = request_factory.get('/')
-        request.session = {} #make sure it has a session associated
-
-        #call the view function
-        resp = index(request)
-
-        #check the response
-        self.assertContains(resp, "<title>Your MVP</title>")
+        resp = index(self.request)
+        self.assertTemplateUsed(resp, "index.html")
 
     def test_returns_exact_html(self):
-        index = self.client.get("/")
-        self.assertEquals(index.content,
+        resp = index(self.request)
+        self.assertEquals(resp.content,
                           render_to_response("index.html").content)
 
     def test_index_handles_logged_in_user(self):
@@ -46,17 +41,16 @@ class MainPageTests(TestCase):
             )
         user.save()
         
-        #create a Mock request object, so we can manipulate the session
-        from django.test import RequestFactory
-        request_factory = RequestFactory()
-        request = request_factory.get('/')
         #create a session that appears to have a logged in user
-        request.session = {"user" : "1"}
+        self.request.session = {"user" : "1"}
 
         #request the index page
-        resp = index(request)
+        resp = index(self.request)
 
+        #ensure we return the state of the session back to normal so
+        #we don't affect other tests
+        self.request.session = {}
+        
         #verify it returns the page for the logged in user
-        #self.assertTemplateUsed(resp, 'user.html')
         expectedHtml = render_to_response('user.html', {'user':user}).content
         self.assertEquals(resp.content,expectedHtml)
