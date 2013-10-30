58f679399c42a97777d21d4259eee05d40bfae0a
Jeremy Johnson <jeremy@softworks.com.my>
2013-10-30 10:15:23 +0800

    got tests for ContactForm model working

--- django_ecommerce/contact/tests.py
+++ django_ecommerce/contact/tests.py
@@ -1,16 +1,24 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from .models import ContactForm
+from datetime import datetime, timedelta
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class UserModelTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        ContactForm(email = "test@dummy.com", name = "test").save()
+        ContactForm(email = "j@j.com", name = "jj").save()
+        cls.firstUser = ContactForm(email = "first@first.com", name = "first",
+                timestamp=datetime.today() + timedelta(days=2))
+        cls.firstUser.save()
+        #cls.test_user = User(email = "j@j.com", name ='test user')
+        #cls.test_user.save()
+
+    def test_contactform_str_returns_email(self):
+       self.assertEquals("first@first.com", str(self.firstUser))
+
+    def test_ordering(self):
+        contacts = ContactForm.objects.all()
+        self.assertTrue(contacts.ordered)
+        self.assertEquals(self.firstUser, contacts[0])
