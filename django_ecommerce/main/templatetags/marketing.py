0c943abfadeff87ae6e4e00d2f3d3bb4df293131
Jeremy Johnson <jeremy@softworks.com.my>
2014-02-26 05:59:36 +0800

    tag: ch8_inclusiontags finished the inclusion tags

---
+++ django_ecommerce/main/templatetags/marketing.py
@@ -0,0 +1,11 @@
+from django import template
+register = template.Library()
+
+@register.inclusion_tag('circle_item.html')
+def circle_header_item(img_name="yoda.jpg", heading="yoda", caption="yoda",
+                       button_link="register", button_title="View details"):
+    return  {'img' : img_name,
+             'heading'     : heading,
+             'caption'     : caption,
+             'button_link' : button_link,
+             'button_title' : button_title}
