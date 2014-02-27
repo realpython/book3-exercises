f5bf608526ca8b174d84413961f583edb98ad01e
Jeremy Johnson <jeremy@softworks.com.my>
2014-02-28 01:35:10 +0800

    tag: before Chapter 8 exercises

--- django_ecommerce/main/templatetags/main_marketing.py
+++ django_ecommerce/main/templatetags/main_marketing.py
@@ -2,7 +2,7 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('main/templatetags/circle_item.html')
-def circle_header_item(img_name="yoda.jpg", heading="yoda", caption="yoda",
+def marketing__circle_item(img_name="yoda.jpg", heading="yoda", caption="yoda",
                        button_link="register", button_title="View details"):
     return  {'img' : img_name,
              'heading'     : heading,
