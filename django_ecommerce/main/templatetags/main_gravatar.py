33edcfa1d54bfaa93670bf1a4fb83c13d8aaf1ba
Jeremy Johnson <jeremy@softworks.com.my>
2014-03-14 11:22:43 +0800

    updated gravatar support to get just url if desired

--- django_ecommerce/main/templatetags/main_gravatar.py
+++ django_ecommerce/main/templatetags/main_gravatar.py
@@ -6,16 +6,15 @@ register = template.Library()
 
 @register.simple_tag
 def gravatar(email, size=140):
-    url = get_url(email, size)
+    url = gravatar_url(email, size)
     return '''<img class="img-circle" src="%s" height="%s" width="%s"
             alt="user.avatar" />''' % (url, size, size)
 
-def get_url(email, size=140):
+@register.simple_tag
+def gravatar_url(email, size=140):
     default = 'http://starwars.com//img/explore/encyclopedia/characters/anakinskywalker_relationship.png'
     query_params = urlencode([('s', str(size)),
                               ('d', default)])
-
-
 
     return ('http://www.gravatar.com/avatar/' +
            hashlib.md5(email.lower().encode('utf-8')).hexdigest() + 
