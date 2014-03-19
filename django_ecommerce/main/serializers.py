7bb1f3f35bd6e0bf04aa3afd1e2f01c64e46c554
Jeremy Johnson <jeremy@softworks.com.my>
2014-03-19 14:38:28 +0800

    got status_reports collections json view up

--- django_ecommerce/main/serializers.py
+++ django_ecommerce/main/serializers.py
@@ -7,9 +7,6 @@ class RelatedUserField(serializers.RelatedField):
 
     read_only = False
 
-    def to_native(self, value):
-        return str(value)
-
     def from_native(self, data):
         return User.objects.get(email=data)
 
@@ -19,35 +16,5 @@ class StatusReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = StatusReport
         fields = ('id','user','when','status')
-
-
-
-    '''
-    pk = serializers.Field()
-    user = serializers.PrimaryKeyRelatedField(many=False)
-    when = serializers.DateTimeField()
-    status = serializers.CharField(max_length=200)
-    
-
-    def restore_object(self, attrs, instance=None):
-
-        if instance:
-            instance.user = attrs.get('user', instance.user)
-            instance.when = attrs.get('when', instance.when)
-            instance.status = attrs.get('status', instance.status)
-            print ("we returned and instance")
-            return instance
-
-        status = Status_Reports(**attrs)
-
-        try:
-            user = User.object.get(email=attrs['user'])
-            status.user = user
-        except:
-            import pprint
-            pprint.pprint(attrs)
-        
-        return status
-    '''
 
 
