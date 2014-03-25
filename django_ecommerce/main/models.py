1b955e695794ec2df5beea212acb1747bf90a1d6
Jeremy Johnson <jeremy@softworks.com.my>
2014-03-26 02:42:41 +0800

    tag Ch9_ex2 looks good

--- django_ecommerce/main/models.py
+++ django_ecommerce/main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from payments.models import User
+from datetime import datetime
 
 # Create your models here.
 class MarketingItem(models.Model):
@@ -14,17 +14,38 @@ class StatusReportQuerySet(models.QuerySet):
         return self.all().order_by('-when')[:20]
 
 class StatusReport(models.Model):
-    user = models.ForeignKey(User)
-    when = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey('payments.User')
+    when = models.DateTimeField(blank=True)
     status = models.CharField(max_length=200)
     objects = StatusReportQuerySet.as_manager()
 
+    def save(self, *args, **kwargs):
+        if self.when is None:
+            self.when = self._getNowNoMicroseconds()
+        super(StatusReport, self).save(*args, **kwargs)
+
+    def _getNowNoMicroseconds(self):
+        '''we want to get time without microseconds so it
+        converts to javascript time correctly'''
+        t = datetime.now()
+        return datetime(t.year, t.month,t.day,t.hour,
+                        t.minute,t.second,0,t.tzinfo)
+    
 class Announcement(models.Model):
 
     when = models.DateTimeField(auto_now=True)
     img = models.CharField(max_length=255, null=True)
     vid = models.URLField(null=True)
     info = models.TextField()
+
+class Badge(models.Model):
+
+    img = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+
+    class Meta:
+        ordering = ('name',)
 
 
 
