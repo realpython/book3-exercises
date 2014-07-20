f995c6018808d1f3eae7e80c44c11b1ec029a8b1
Michael Herman <hermanmu@gmail.com>
2014-07-20 15:22:12 -0500

    chp 7

--- _chapters/chp7/django_ecommerce/payments/tests.py
+++ _chapters/chp7/django_ecommerce/payments/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
+from django import forms
 from payments.models import User
+from payments.forms import SigninForm, UserForm
+import unittest
 
 
 class UserModelTest(TestCase):
@@ -14,3 +17,70 @@ class UserModelTest(TestCase):
 
     def test_get_by_id(self):
         self.assertEquals(User.get_by_id(1), self.test_user)
+
+
+class FormTesterMixin():
+
+    def assertFormError(self, form_cls, expected_error_name,
+                        expected_error_msg, data):
+
+        from pprint import pformat
+        test_form = form_cls(data=data)
+        #if we get an error then the form should not be valid
+        self.assertFalse(test_form.is_valid())
+
+        self.assertEquals(
+            test_form.errors[expected_error_name],
+            expected_error_msg,
+            msg="Expected {} : Actual {} : using data {}".format(
+                test_form.errors[expected_error_name],
+                expected_error_msg, pformat(data)
+            )
+        )
+
+
+class FormTests(unittest.TestCase, FormTesterMixin):
+
+    def test_signin_form_data_validation_for_invalid_data(self):
+        invalid_data_list = [
+            {'data': {'email': 'j@j.com'},
+             'error': ('password', [u'This field is required.'])},
+            {'data': {'password': '1234'},
+             'error': ('email', [u'This field is required.'])}
+        ]
+
+        for invalid_data in invalid_data_list:
+            self.assertFormError(SigninForm,
+                                 invalid_data['error'][0],
+                                 invalid_data['error'][1],
+                                 invalid_data["data"])
+
+    def test_user_form_passwords_match(self):
+        form = UserForm(
+            {
+                'name': 'jj',
+                'email': 'j@j.com',
+                'password': '1234',
+                'ver_password': '1234',
+                'last_4_digits': '3333',
+                'stripe_token': '1'}
+        )
+
+        # This will throw an error if it doesn't clean correctly
+        self.assertIsNotNone(form.clean())
+
+    def test_user_form_passwords_dont_match_throws_error(self):
+        form = UserForm(
+            {
+                'name': 'jj',
+                'email': 'j@j.com',
+                'password': '234',
+                'ver_password': '1234',
+                'last_4_digits': '3333',
+                'stripe_token': '1'
+            }
+        )
+
+        self.assertRaisesMessage(
+            forms.ValidationError, "Passwords do not match", form.clean
+        )
