eb1be9db991db4b3b65431e47e3ceab089121010
Michael Herman <hermanmu@gmail.com>
2014-07-21 19:47:48 -0500

    chp8

---
+++ _chapters/chp8/django_ecommerce/contact/tests.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from .models import ContactForm
+from datetime import datetime, timedelta
+
+
+class UserModelTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        ContactForm(email="test@dummy.com", name="test").save()
+        ContactForm(email="j@j.com", name="jj").save()
+        cls.firstUser = ContactForm(
+            email="first@first.com",
+            name="first",
+            timestamp=datetime.today() + timedelta(days=2)
+        )
+        cls.firstUser.save()
+        #cls.test_user=User(email="j@j.com", name ='test user')
+        #cls.test_user.save()
+
+    def test_contactform_str_returns_email(self):
+        self.assertEquals("first@first.com", str(self.firstUser))
+
+    def test_ordering(self):
+        contacts = ContactForm.objects.all()
+        self.assertEquals(self.firstUser, contacts[0])
---
+++ _chapters/chp8/django_ecommerce/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render_to_response
+from payments.models import User
+
+
+def index(request):
+    uid = request.session.get('user')
+    if uid is None:
+        return render_to_response('index.html')
+    else:
+        return render_to_response(
+            'user.html', {'user': User.get_by_id(uid)}
+        )
