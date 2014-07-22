139ff2fa34badd690b4e80b0a975ecceffeb9876
Michael Herman <hermanmu@gmail.com>
2014-07-22 12:47:15 -0500

    chp9

--- _chapters/chp9/django_ecommerce/payments/tests.py
+++ _chapters/chp9/django_ecommerce/payments/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase, RequestFactory
 from django.shortcuts import render_to_response
+import django_ecommerce.settings as settings
+from payments.views import soon, register
 from payments.models import User
 from payments.forms import SigninForm, UserForm, CardForm
 import unittest
+import mock
+
 
 ########################
 #### Testing Models ####
@@ -189,3 +193,124 @@ class SignOutPageTests(TestCase, ViewTesterMixin):
     def setUp(self):
         #sign_out clears the session, so let's reset it everytime
         self.request.session = {"user": "dummy"}
+
+
+class RegisterPageTests(TestCase, ViewTesterMixin):
+
+    @classmethod
+    def setUpClass(cls):
+        html = render_to_response(
+            'register.html',
+            {
+                'form': UserForm(),
+                'months': range(1, 12),
+                'publishable': settings.STRIPE_PUBLISHABLE,
+                'soon': soon(),
+                'user': None,
+                'years': range(2011, 2036),
+            }
+        )
+        ViewTesterMixin.setupViewTester(
+            '/register',
+            register,
+            html.content,
+        )
+
+    def setUp(self):
+        request_factory = RequestFactory()
+        self.request = request_factory.get(self.url)
+
+    def test_invalid_form_returns_registration_page(self):
+
+        with mock.patch('payments.forms.UserForm.is_valid') as user_mock:
+
+            user_mock.return_value = False
+
+            self.request.method = 'POST'
+            self.request.POST = None
+            resp = register(self.request)
+            self.assertEquals(resp.content, self.expected_html)
+
+            # make sure that we did indeed call our is_valid function
+            self.assertEquals(user_mock.call_count, 1)
+
+    def test_registering_new_user_returns_succesfully(self):
+
+        self.request.session = {}
+        self.request.method = 'POST'
+        self.request.POST = {
+            'email': 'python@rocks.com',
+            'name': 'pyRock',
+            'stripe_token': '...',
+            'last_4_digits': '4242',
+            'password': 'bad_password',
+            'ver_password': 'bad_password',
+        }
+
+        with mock.patch('stripe.Customer') as stripe_mock:
+
+            config = {'create.return_value': mock.Mock()}
+            stripe_mock.configure_mock(**config)
+
+            resp = register(self.request)
+            self.assertEquals(resp.content, "")
+            self.assertEquals(resp.status_code, 302)
+            self.assertEquals(self.request.session['user'], 1)
+
+            # verify the user was actually stored in the database.
+            # if the user is not there this will throw an error
+            User.objects.get(email='python@rocks.com')
+
+    def test_registering_user_twice_cause_error_msg(self):
+
+        #create a user with same email so we get an integrity error
+        user = User(name='pyRock', email='python@rocks.com')
+        user.save()
+
+        #now create the request used to test the view
+        self.request.session = {}
+        self.request.method = 'POST'
+        self.request.POST = {
+            'email': 'python@rocks.com',
+            'name': 'pyRock',
+            'stripe_token': '...',
+            'last_4_digits': '4242',
+            'password': 'bad_password',
+            'ver_password': 'bad_password',
+        }
+
+        #create our expected form
+        expected_form = UserForm(self.request.POST)
+        expected_form.is_valid()
+        expected_form.addError('python@rocks.com is already a member')
+
+        #create the expected html
+        html = render_to_response(
+            'register.html',
+            {
+                'form': expected_form,
+                'months': range(1, 12),
+                'publishable': settings.STRIPE_PUBLISHABLE,
+                'soon': soon(),
+                'user': None,
+                'years': range(2011, 2036),
+            }
+        )
+
+        #mock out stripe so we don't hit their server
+        with mock.patch('stripe.Customer') as stripe_mock:
+
+            config = {'create.return_value': mock.Mock()}
+            stripe_mock.configure_mock(**config)
+
+            #run the test
+            resp = register(self.request)
+
+            #verify that we did things correctly
+            self.assertEquals(resp.content, html.content)
+            self.assertEquals(resp.status_code, 200)
+            self.assertEquals(self.request.session, {})
+
+            #assert there is only one record in the database.
+            users = User.objects.filter(email="python@rocks.com")
+            self.assertEquals(len(users), 1)
