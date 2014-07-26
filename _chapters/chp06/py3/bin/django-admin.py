e5db72dbf9c454a7bed3614595add9f4585a7e77
Michael Herman <hermanmu@gmail.com>
2014-07-25 20:08:46 -0500

    removed virtualenv

--- _chapters/chp06/py3/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/michaelherman/Documents/repos/realpython/book3-exercises/_chapters/chp11/py3/bin/python3.4
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
