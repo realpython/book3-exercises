f5fbf1d64c8bcbe70425e24d584bbbbf4c5e7de0
Michael Herman <hermanmu@gmail.com>
2014-08-28 17:39:29 -0700

    angular primer

---
+++ _chapters/chp11/django_ecommerce/payments/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from main.models import Badge
+from datetime import datetime
+
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    #password field defined in base class
+    last_4_digits = models.CharField(max_length=4, blank=True, null=True)
+    stripe_id = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    rank = models.CharField(max_length=50, default="Padwan")
+    badges = models.ManyToManyField(Badge)
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
+    @classmethod
+    def get_by_id(cls, uid):
+        return User.objects.get(pk=uid)
+
+    @classmethod
+    def create(cls, name, email, password, last_4_digits, stripe_id=''):
+        new_user = cls(name=name, email=email,
+                       last_4_digits=last_4_digits, stripe_id=stripe_id)
+        new_user.set_password(password)
+
+        new_user.save()
+        return new_user
+
+
+class UnpaidUsers(models.Model):
+    email = models.CharField(max_length=255, unique=True)
+    last_notification = models.DateTimeField(default=datetime.now())
---
+++ _chapters/chp11/django_ecommerce/payments/views.py
@@ -0,0 +1,163 @@
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from payments.forms import SigninForm, CardForm, UserForm
+from payments.models import User, UnpaidUsers
+import django_ecommerce.settings as settings
+import stripe
+import datetime
+import socket
+
+stripe.api_key = settings.STRIPE_SECRET
+
+
+def soon():
+    soon = datetime.date.today() + datetime.timedelta(days=30)
+    return {'month': soon.month, 'year': soon.year}
+
+
+def sign_in(request):
+    user = None
+    if request.method == 'POST':
+        form = SigninForm(request.POST)
+        if form.is_valid():
+            results = User.objects.filter(email=form.cleaned_data['email'])
+            if len(results) == 1:
+                if results[0].check_password(form.cleaned_data['password']):
+                    request.session['user'] = results[0].pk
+                    return HttpResponseRedirect('/')
+                else:
+                    form.addError('Incorrect email address or password')
+            else:
+                form.addError('Incorrect email address or password')
+    else:
+        form = SigninForm()
+
+    print(form.non_field_errors())
+
+    return render_to_response(
+        'payments/sign_in.html',
+        {
+            'form': form,
+            'user': user
+        },
+        context_instance=RequestContext(request)
+    )
+
+
+def sign_out(request):
+    del request.session['user']
+    return HttpResponseRedirect('/')
+
+
+def register(request):
+    user = None
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+
+            #update based on your billing method (subscription vs one time)
+            customer = Customer.create(
+                email=form.cleaned_data['email'],
+                description=form.cleaned_data['name'],
+                card=form.cleaned_data['stripe_token'],
+                plan="gold",
+            )
+            # customer = stripe.Charge.create(
+            #     description=form.cleaned_data['email'],
+            #     card=form.cleaned_data['stripe_token'],
+            #     amount="5000",
+            #     currency="usd"
+            # )
+
+            cd = form.cleaned_data
+            from django.db import transaction
+
+            try:
+                with transaction.atomic():
+                    user = User.create(cd['name'], cd['email'], cd['password'],
+                                       cd['last_4_digits'], stripe_id="")
+
+                    if customer:
+                        user.stripe_id = customer.id
+                        user.save()
+                    else:
+                        UnpaidUsers(email=cd['email']).save()
+
+            except IntegrityError:
+                import traceback
+                form.addError(cd['email'] + ' is already a member' +
+                              traceback.format_exc())
+                user = None
+            else:
+                request.session['user'] = user.pk
+                return HttpResponseRedirect('/')
+
+    else:
+        form = UserForm()
+
+    return render_to_response(
+        'payments/register.html',
+        {
+            'form': form,
+            'months': list(range(1, 12)),
+            'publishable': settings.STRIPE_PUBLISHABLE,
+            'soon': soon(),
+            'user': user,
+            'years': list(range(2011, 2036)),
+        },
+        context_instance=RequestContext(request)
+    )
+
+
+def edit(request):
+    uid = request.session.get('user')
+
+    if uid is None:
+        return HttpResponseRedirect('/')
+
+    user = User.objects.get(pk=uid)
+
+    if request.method == 'POST':
+        form = CardForm(request.POST)
+        if form.is_valid():
+
+            customer = stripe.Customer.retrieve(user.stripe_id)
+            customer.card = form.cleaned_data['stripe_token']
+            customer.save()
+
+            user.last_4_digits = form.cleaned_data['last_4_digits']
+            user.stripe_id = customer.id
+            user.save()
+
+            return HttpResponseRedirect('/')
+
+    else:
+        form = CardForm()
+
+    return render_to_response(
+        'payments/edit.html',
+        {
+            'form': form,
+            'publishable': settings.STRIPE_PUBLISHABLE,
+            'soon': soon(),
+            'months': list(range(1, 12)),
+            'years': list(range(2011, 2036))
+        },
+        context_instance=RequestContext(request)
+    )
+
+
+class Customer(object):
+
+    @classmethod
+    def create(cls, billing_method="subscription", **kwargs):
+        try:
+            if billing_method == "subscription":
+                return stripe.Customer.create(**kwargs)
+            elif billing_method == "one_time":
+                return stripe.Charge.create(**kwargs)
+        except (socket.error, stripe.APIConnectionError,
+                stripe.InvalidRequestError):
+            return None
