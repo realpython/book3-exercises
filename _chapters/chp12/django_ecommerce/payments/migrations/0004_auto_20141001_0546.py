2612b9d67d855be8b66a1872dbed5d85fa47bbaf
Jeremy Johnson <jeremy@softworks.com.my>
2014-10-01 19:20:24 +0800

    tag:final_migrations finished last exercise

---
+++ _chapters/chp12/django_ecommerce/payments/migrations/0004_auto_20141001_0546.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('payments', '0003_initial_data'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='user',
+            name='bigCoID',
+            field=models.CharField(max_length=50, default=datetime.datetime(2014, 10, 1, 5, 46, 4, 329384), unique=True),
+            preserve_default=False,
+        ),
+        migrations.AlterField(
+            model_name='unpaidusers',
+            name='last_notification',
+            field=models.DateTimeField(default=datetime.datetime(2014, 10, 1, 5, 44, 20, 437687)),
+        ),
+    ]
--- _chapters/chp12/django_ecommerce/payments/models.py
+++ _chapters/chp12/django_ecommerce/payments/models.py
@@ -14,6 +14,7 @@ class User(AbstractBaseUser):
     updated_at = models.DateTimeField(auto_now=True)
     rank = models.CharField(max_length=50, default="Padwan")
     badges = models.ManyToManyField(Badge)
+    bigCoID = models.CharField(max_length=50, unique=True)
 
     USERNAME_FIELD = 'email'
 
