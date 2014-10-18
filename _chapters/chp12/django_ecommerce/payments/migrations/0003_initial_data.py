9ac6e553cf6a7b27913c8e6c526364bab6bc2735
Jeremy Johnson <jeremy@softworks.com.my>
2014-10-18 17:26:41 +0800

    updated migration file so they don't fail

--- _chapters/chp12/django_ecommerce/payments/migrations/0003_initial_data.py
+++ _chapters/chp12/django_ecommerce/payments/migrations/0003_initial_data.py
@@ -2,19 +2,21 @@
 from __future__ import unicode_literals
 from django.db import migrations
 from payments.models import User
+from django.contrib.auth.hashers import make_password
 
 
 def create_default_user(apps, schema_editor):
+    new_user = apps.get_model("payments", "User")
     try:
-        vader = User.objects.get(email="darth@mec.com")
+        vader = new_user.objects.get(email="darth@mec.com")
         vader.delete()
-    except User.DoesNotExist:
+    except new_user.DoesNotExist:
         pass
 
-    new_user = User.create(
+    u = new_user(
         name='vader', email="darth@mec.com",
-        password="darkside", last_4_digits="1234"
-    )
+        last_4_digits="1234", password= make_password("darkside")
+    ).save()
 
 
 class Migration(migrations.Migration):
