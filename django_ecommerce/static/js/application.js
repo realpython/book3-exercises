37112d8883352797d3966ec17118fe45649e8be7
Michael Herman <hermanmu@gmail.com>
2014-11-25 22:08:24 -0700

    updated chp15

--- django_ecommerce/static/js/application.js
+++ django_ecommerce/static/js/application.js
@@ -1,36 +1,36 @@
 $(function() {
 
-  $("#user_form").submit(function() {
-    if ( $("#credit-card").is(":visible")) {
-      var form = this;
-      var card = {
-        number:   $("#credit_card_number").val(),
-        expMonth: $("#expiry_month").val(),
-        expYear:  $("#expiry_year").val(),
-        cvc:      $("#cvv").val()
-      };
+  // $("#user_form").submit(function() {
+  //   if ( $("#credit-card").is(":visible")) {
+  //     var form = this;
+  //     var card = {
+  //       number:   $("#credit_card_number").val(),
+  //       expMonth: $("#expiry_month").val(),
+  //       expYear:  $("#expiry_year").val(),
+  //       cvc:      $("#cvv").val()
+  //     };
 
-      Stripe.createToken(card, function(status, response) {
-        if (status === 200) {
-          console.log(status, response);
-          $("#credit-card-errors").hide();
-          $("#last_4_digits").val(response.card.last4);
-          $("#stripe_token").val(response.id);
-          form.submit();
-        } else {
-          $("#stripe-error-message").text(response.error.message);
-          $("#credit-card-errors").show();
-          $("#user_submit").attr("disabled", false);
-        }
-      });
-      
-      return false;
-      
-    } 
-    
-    return true
-    
-  });
+  //     Stripe.createToken(card, function(status, response) {
+  //       if (status === 200) {
+  //         console.log(status, response);
+  //         $("#credit-card-errors").hide();
+  //         $("#last_4_digits").val(response.card.last4);
+  //         $("#stripe_token").val(response.id);
+  //         form.submit();
+  //       } else {
+  //         $("#stripe-error-message").text(response.error.message);
+  //         $("#credit-card-errors").show();
+  //         $("#user_submit").attr("disabled", false);
+  //       }
+  //     });
+
+  //     return false;
+
+  //   }
+
+  //   return true
+
+  // });
 
   $("#change-card a").click(function() {
     $("#change-card").hide();
@@ -38,7 +38,7 @@ $(function() {
     $("#credit_card_number").focus();
     return false;
   });
-  
+
   //show status
   $("#show-achieve").click(function() {
     a = $("#achievements");
