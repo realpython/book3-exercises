a7bc8accbac137fea7f3231140fafcd2b7f793d7
Jeremy Johnson <jeremy@softworks.com.my>
2014-12-12 10:08:34 +0800

    fixed user map stuff that I accidently overwrote

--- django_ecommerce/static/js/userMapCtrl.js
+++ django_ecommerce/static/js/userMapCtrl.js
@@ -17,14 +17,6 @@ mecApp.controller('UserMapCtrl', function($scope, locations) {
     center: {
         latitude: 38.062056, 
         longitude: -122.643380
-  38.062056,-122.643380]
-38.062056,-122.643380
-
-
-db.user_location.update({_id:ObjectId("53f6d89059b1e9d1493bea5f"), {$set : {"email" : "nnn@nn.com", "name" : "nnn", "location" : { "type" : "Point", "coordinates" : [38.062056, -122.643380] }, "mappoint_id" : 6 }}});
-
-
-
     },
     zoom: 2,
     options: { 
