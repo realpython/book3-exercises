88203130edad15ddab1888833dab01b0097634c4
Jeremy Johnson <jeremy@softworks.com.my>
2015-01-12 17:07:02 +0800

    fixed issues with mongo mongo chapter

--- _chapters/chp16/django_ecommerce/django_ecommerce/urls.py
+++ _chapters/chp16/django_ecommerce/django_ecommerce/urls.py
@@ -4,11 +4,12 @@ from payments import views
 from main.urls import urlpatterns as main_json_urls
 from djangular_polls.urls import urlpatterns as djangular_polls_json_urls
 from payments.urls import urlpatterns as payments_json_urls
-
+from usermap.urls import urlpatterns as map_json_urls
 
 admin.autodiscover()
 main_json_urls.extend(djangular_polls_json_urls)
 main_json_urls.extend(payments_json_urls)
+main_json_urls.extend(map_json_urls)
 
 
 urlpatterns = patterns(
