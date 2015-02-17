06e2f746421d4cc00e347b7af3e09fee42ceb2ca
Jeremy Johnson <jeremy@softworks.com.my>
2015-02-17 08:17:34 +0800

    fabfile for auto-deployment

---
+++ deploy/fabfile.py
@@ -0,0 +1,23 @@
+from fabric.api import *
+
+env.hosts = ['128.199.202.178']
+env.user = 'root'
+
+
+def get_latest():
+    with cd("/opt/mec_env/mec_app"):
+        run("git pull")
+    with cd("/opt/mec_env/mec_app/django_ecommerce"):
+        with prefix("source /opt/mec_env/bin/activate"):
+            run("pip install -r ../requirements.txt")
+            run("./manage.py migrate")
+            run("./manage.py collectstatic")
+
+def update_config():
+    with cd("/opt/mec_env/mec_app/deploy"):
+        run("cp settings_prod.py ../django_ecommerce/django_ecommerce/")
+        run("cp supervisor/mec.conf /etc/supervisor/conf.d/")
+        run("cp nginx/sites-avaliable/mec /etc/nginx/sites-available/")
+        run("/etc/init.d/supervisor restart")
+        run("/etc/init.d/nginx restart")
+
