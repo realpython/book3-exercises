3cbe9824b192a71e18ade6fd481222dacd09e540
Jeremy Johnson <jeremy@softworks.com.my>
2015-06-05 10:33:11 +0800

    got tests working for 1.8 still have some connection already closed issue

--- _chapters/chp11/tests/main/testSerializers.py
+++ _chapters/chp11/tests/main/testSerializers.py
@@ -5,24 +5,28 @@ from main.serializers import StatusReportSerializer
 from rest_framework.renderers import JSONRenderer
 from collections import OrderedDict
 from rest_framework.parsers import JSONParser
-from rest_framework.compat import BytesIO
-import datetime
+from django.utils.six import BytesIO
 
 
 class StatusReportSerializer_Tests(TestCase):
 
     @classmethod
     def setUpClass(cls):
+        super().setUpClass()
         cls.u = User(name="test", email="test@test.com")
         cls.u.save()
 
         cls.new_status = StatusReport(user=cls.u, status="hello world")
         cls.new_status.save()
 
+        when = cls.new_status.when.isoformat()
+        if when.endswith('+00:00'):
+            when = when[:-6] + 'Z'
+
         cls.expected_dict = OrderedDict([
             ('id', cls.new_status.id),
             ('user', cls.u.email),
-            ('when', cls.new_status.when),
+            ('when', when),
             ('status', 'hello world'),
         ])
 
@@ -30,6 +34,7 @@ class StatusReportSerializer_Tests(TestCase):
     def tearDownClass(cls):
         cls.u.delete()
         cls.new_status.delete()
+        super().tearDownClass()
 
     def test_model_to_dictionary(self):
         serializer = StatusReportSerializer(self.new_status)
@@ -42,18 +47,29 @@ class StatusReportSerializer_Tests(TestCase):
         self.assertEquals(expected_json, content)
 
     def test_json_to_StatusReport(self):
+        json = JSONRenderer().render(self.expected_dict)
+        stream = BytesIO(json)
+        data = JSONParser().parse(stream)
 
+        serializer = StatusReportSerializer(self.new_status, data=data)
+        self.assertTrue(serializer.is_valid())
+    
+        status = serializer.save()
+        self.assertEqual(self.new_status, status)
+        self.assertEqual(self.new_status.id, status.id)
+        self.assertEqual(self.new_status.status, status.status)
+        self.assertEqual(self.new_status.when, status.when)
+        self.assertEqual(self.new_status.user, status.user)
+
+    def test_json_to_new_StatusReport(self):
         json = JSONRenderer().render(self.expected_dict)
         stream = BytesIO(json)
         data = JSONParser().parse(stream)
 
         serializer = StatusReportSerializer(data=data)
         self.assertTrue(serializer.is_valid())
-        self.assertEqual(self.new_status.status, serializer.object.status)
-        self.assertAlmostEqual(
-            self.new_status.when,
-            serializer.object.when,
-            delta=datetime.timedelta(seconds=1)
-        )
-
-        self.assertEqual(self.new_status.user, serializer.object.user)
+   
+        status = serializer.save()
+        self.assertEqual(self.new_status.status, status.status)
+        self.assertIsNotNone(status.when)
+        self.assertEqual(self.new_status.user, status.user)
