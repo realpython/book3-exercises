78a2bc919f27ca22c9270e08afd84f964f86408e
Jeremy Johnson <jeremy@softworks.com.my>
2015-06-06 20:34:23 +0800

    tests working for chp18

---
+++ _chapters/chp17/django_ecommerce/payments/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('payments', '0007_auto_20150606_0307'),
+        ('payments', '0008_auto_20150311_1313'),
+    ]
+
+    operations = [
+    ]
