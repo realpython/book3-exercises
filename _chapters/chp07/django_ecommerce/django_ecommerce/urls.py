4aa2d3f09509230e42f09c5528ad0618cae9127a
j1z0 <jeremy@softworks.com.my>
2016-08-22 21:41:14 -0700

    code for the end of chp06 / start of chp07

---
+++ _chapters/chp07/django_ecommerce/django_ecommerce/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import include, url
+from payments import views
+from django.contrib import admin
+from main.views import index as main_index
+from contact.views import contact
+
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', main_index, name='home'),
+    url(r'^pages/', include('django.contrib.flatpages.urls')),
+    url(r'^contact/', contact, name='contact'),
+
+    # user registration/authentication
+    url(r'^sign_in$', views.sign_in, name='sign_in'),
+    url(r'^sign_out$', views.sign_out, name='sign_out'),
+    url(r'^register$', views.register, name='register'),
+    url(r'^edit$', views.edit, name='edit'),
+]
