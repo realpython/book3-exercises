fb8b7e24eb812ca24fa37c1c98898cb0fccda4aa
j1z0 <jeremy@softworks.com.my>
2016-12-03 15:18:58 -0500

    StatusReportSerializer working; on to the view

---
+++ _chapters/chp13/django_ecommerce/main/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from main.models import StatusReport
+from payments.models import User
+
+class RelatedUserField(serializers.RelatedField):
+
+    read_only = False
+
+    def to_representation(self, value):
+        return value.email
+
+    def to_internal_value(self, data):
+        return User.objects.get(email=data)
+
+
+class StatusReportSerializer(serializers.ModelSerializer):
+    user = RelatedUserField(queryset=User.objects.all())
+
+    class Meta:
+        model = StatusReport
+        fields = ('id', 'user', 'when', 'status')
+
+
